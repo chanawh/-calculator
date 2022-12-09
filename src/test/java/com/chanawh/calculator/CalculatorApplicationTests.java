afda046ea5e9aa8ef6329731f132a0dd99843e06
chanawh <thechanwh@gmail.com>
2022-12-09 04:25:12 +0000

    Add Spring Boot skeleton

---
+++ src/test/java/com/chanawh/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanawh.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
